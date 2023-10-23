f5ada9d9bcb12e37be8768807bc43f03d1d6b928
Victor Ananjevsky <victor@sanana.kiev.ua>
2023-10-23 16:21:53 +0300

    correctly handle empty or nonexisting directory in qlaunch script

--- fvwm/scripts/qlaunch.sh
+++ fvwm/scripts/qlaunch.sh
@@ -3,8 +3,11 @@
 
 isize=$(gsettings get ydesk.panel isize)
 
+qldir="${XDG_DATA_HOME:-$HOME/.share}/ydesk/qlaunch/"
+mkdir -p "$qldir"
+
 echo "*FvwmPanel: (1x1)"
-for f in ${XDG_DATA_HOME:-$HOME/.share}/ydesk/qlaunch/*.desktop; do
+for f in $(find "$qldir" -type f -name "*.desktop"); do
     eval $(grep -E '^[[:blank:]]*(Icon|Name|Exec)=' "$f" | sed -r 's/=(.*)$/="\1"/')
     ficon=$(yad-tools --icon --size=$isize ${Icon:-system-run})
     echo "*FvwmPanel: (25x1, Padding 2 2, ActiveColorset 2, Icon \"$ficon\", TipsLabel \"$Name\", Action Exec exec ${Exec%% \%*})"
