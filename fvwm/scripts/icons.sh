dad5e02d3937b45051396da6be39f1e62315b73b
Victor Ananjevsky <victor@sanana.kiev.ua>
2024-01-06 12:27:33 +0200

    update default icons

--- fvwm/scripts/icons.sh
+++ fvwm/scripts/icons.sh
@@ -5,11 +5,11 @@
 [[ $1 == -f ]] && rm -rf ${XDG_DATA_HOME:-$HOME/.share}/ydesk/fvwm/icons
 
 declare -A common=([edit]=accessories-text-editor [media]=drive-removable-media
-    [info]=dialog-information [lock]=system-lock-screen [mixer]=multimedia-volume-control [notes]=ynotes
-    [prefs]=preferences-system [recent]=document-open-recent [restart]=view-refresh [run]=system-run
-    [search]=system-search [shot]=applets-screenshooter [shutdown]=system-shutdown 
-    [settings]=preferences-other [sysmon]=utilities-system-monitor [term]=utilities-terminal
-    [web]=web-browser [winclose]=window-close [y]=ydesk
+    [info]=dialog-information [lock]=system-lock-screen [mixer]=multimedia-volume-control 
+    [menu]=open-menu [notes]=ynotes [prefs]=preferences-system [recent]=document-open-recent 
+    [restart]=view-refresh [run]=system-run [search]=system-search [shot]=applets-screenshooter
+    [shutdown]=system-shutdown [settings]=preferences-other [sysmon]=utilities-system-monitor
+    [term]=utilities-terminal [web]=web-browser [winclose]=window-close [y]=ydesk
     [play]=media-palyback-start [pause]=media-playback-pause [prev]=media-skip-backward 
     [next]=media-skip-forward [stop]=media-playback-stop)
 
